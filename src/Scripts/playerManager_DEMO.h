be7e6766e485e62cad70d3b5af8ce3341ece2e55
battletag13 <matthewlinplusprogramming@gmail.com>
2020-03-21 12:24:34 -0700

    Added (clearly marked) demo code to the engine in prep for first release

---
+++ src/Scripts/playerManager_DEMO.h
@@ -0,0 +1,38 @@
+//  FastEngine
+//
+//  Created by Matthew Lin.
+//  Contact: matthewlinplusprogramming@gmail.com
+//
+//  Demo playerManager header which allows the player to control a game object.
+//
+//  Copyright © 2020 Matthew Lin. All rights reserved.
+//
+#ifndef FAST_ENGINE_DEMO_PLAYER_MANAGER_H_
+#define FAST_ENGINE_DEMO_PLAYER_MANAGER_H_
+
+#include "engineAPI.h"
+
+class PlayerManager : public Component {
+public:
+  void update() {
+    velocity = Vector2D(0, 0);
+    if (input::getButton(SDLK_s)) {
+      velocity.y = 1;
+    } else if (input::getButton(SDLK_w)) {
+      velocity.y = -1;
+    }
+
+    if (input::getButton(SDLK_a)) {
+      velocity.x = -1;
+    } else if (input::getButton(SDLK_d)) {
+      velocity.x = 1;
+    }
+
+    gameObject->getComponent<Transform>().translate(time::deltaTime * velocity);
+  }
+
+private:
+  Vector2D velocity;
+};
+
+#endif
--- src/Scripts/start.cpp
+++ src/Scripts/start.cpp
@@ -13,10 +13,17 @@
 #include "engineAPI.h"
 
 // Other includes
+#include "playerManager_DEMO.h"
 
 // Used for addGameObject
 Manager *manager = Manager::getInstance();
 
 void start() {
   // Game starts here!
+
+  // Demo code
+  GameObject &player = manager->addGameObject();
+  player.addComponent<SpriteRenderer>("src/Res/player.png", Vector2D(64, 64));
+  player.addComponent<PlayerManager>();
+  // End demo code
 }
